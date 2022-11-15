cda46acb8b995024bc1bb4f60a734daa56f8bf40
Maria Soledad Lanfranconi <pennymsl@gmail.com>
2022-11-15 16:23:04 -0300

    fusion del shopping cart al index

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuMobileIcon = document.querySelector('.menu-icon')
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuShoppingCart = document.querySelector('.navbar-shoppingcart')
+const aside = document.querySelector('.product-detail')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 menuMobileIcon.addEventListener('click', toggleMobileMenu)
+menuShoppingCart.addEventListener('click', toggleShoppingCart)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuclosed = desktopMenu.classList.contains('inactive')
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
+    if (!isDesktopMenuclosed) {
+        desktopMenu.classList.add('inactive')
+    }
 }
