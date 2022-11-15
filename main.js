a220d7d1edaf9910bccd587f0590c8964fdd0565
Maria Soledad Lanfranconi <pennymsl@gmail.com>
2022-11-15 11:18:22 -0300

    fusion del menu para mobile en el index

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuMobileIcon = document.querySelector('.menu-icon')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
+menuMobileIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
